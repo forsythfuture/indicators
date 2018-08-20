b69010ce5ca023f00b45ac69be0df72e6035e8b5
forsythfutures <42545602+forsythfutures@users.noreply.github.com>
2018-08-20 14:54:49 -0400

    Fix typo

--- acs_functions.R
+++ acs_functions.R
@@ -1,4 +1,4 @@
 ########################################################################################################
-# This file contains commonly used functions to access ACS data with the tidycensus and acs packges.
+# This file contains commonly used functions to access ACS data with the tidycensus and acs packages.
 # The functions import and manipulate ACS data.
 ########################################################################################################
