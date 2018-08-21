1ed5e244fecc6c04594dae703599074bb44d55cd
shanejorr <shane.j.orr@gmail.com>
2018-08-21 13:12:58 -0400

    Updated gender inequality file to import ACS data and create csv file.

--- social_justice/gender_income_inequality.R
+++ social_justice/gender_income_inequality.R
@@ -1,72 +1,27 @@
-library(tidycensus)
-library(tidyverse)
+################################################################
+# This file creates the data set for gender inequality in pay.
+################################################################
 
-# load and save variables
-var_names16 <- load_variables(2016, "acs1")
 
-# find all B20017 tables and variables
-gender_income_vars <- var_names16 %>% 
-  filter(str_detect(.$name, "^B20017")) %>%
-  # remove letter at the end of the varible names, since these letters are not at the end of the data table
-  mutate(name = str_sub(.$name, 1, -2),
-         # for variable decription, replace '!!' with new line '\n'
-         label = str_replace_all(.$label, '!!', '\n'),
-         # only keep the ethnicity part of the c'concept' variable
-         # this part is at the end of the line, and surrounded by brackets
-         # we need to keep this part, so gender pay ratios can be isolated by ethnicity
-         concept = str_match(.$concept, "[(][A-Z| |,]*?[)]$")) %>%
-  # remove brackets - first and last character - from ethnicity
-  mutate(concept = str_sub(.$concept, 2, -2)) %>%
-  # NA values for ethnicity represent totals, change NA values to 'all'
-  mutate(concept = replace_na(.$concept, 'All')) %>%
-  # rename 'concept' column
-  rename(ethnicity = concept)
+# import custom ACS functions
+# This loads the tidyverse and tidycensus packages,
+# so they do not need to be loaded in this file
+source('acs_functions.R')
 
-# remove variables from table names; this leaves us with the different B20017 tables
-# the varible names are after the '_'; therefore use regular expressions to remove characters after '_';
-# then store the distinct table names in a vector
-gender_income_tables <- str_match(gender_income_vars$name, '(.*)_') %>% # isolte table name
-  .[1:nrow(gender_income_vars), 2] %>% # only keep column with table name
-  unique() # only keep unique values
+table_number <- 'B20017'
 
-# pull data for each table; store date in a list, with each element in the list as a different table
+gender_pay_data <- 'social_justice/data/gender_pay.csv'
 
-# initialize list to store data
-gender_income_list <- list()
+# import B20017 table
+gender_pay <- ff_import_acs_county(table_number, compare$state, compare$county, 2012, 2016)
 
-for (i in seq_along(gender_income_tables)) {
+# save acs data in a csv file
+#write_csv(gender_pay, gender_pay_data)
 
-  # the first dataframe must be created seperately from the others,
-  # and then the other dataframes will bs merged into the first one
-  
-  if (i == 1) {
-    
-    # create first dataframe
-    # others will be merged into this one
-    gender_income <- get_acs(geography = "county", 
-                             table = gender_income_tables[[i]], year = 2016, survey = 'acs1',
-                             state = "NC", county = "Forsyth") %>%
-      # add variable descriptions to table
-      left_join(gender_income_vars, by = c('variable' = 'name'))
-    
-  } else {
-    
-    # create dataframes for other tables, and merge them into the main dataframe
-    gender_income_single <- get_acs(geography = "county", 
-                                    table = gender_income_tables[[i]], year = 2016, survey = 'acs1',
-                                    state = "NC", county = "Forsyth")  %>%
-      # add variable descriptions to table
-      left_join(gender_income_vars, by = c('variable' = 'name'))
-    
-    gender_income <- gender_income %>%
-      bind_rows(gender_income_single)
-    
-  }
-}
+### In years following 2016, run this code to update the datasets
 
-# create vector of the needed ethnicities, so dataframe cab be filtered for these ethnicities
-ethnicities <- c("All", "WHITE ALONE, NOT HISPANIC OR LATINO", "HISPANIC OR LATINO", "BLACK OR AFRICAN AMERICAN ALONE")
-
-get_acs(geography = "county", 
-        table = gender_income_tables[[i]], year = 2008, survey = 'acs1',
-        state = "NC", county = "Forsyth")
+# # import master csv file of past data, and add current year data
+# gender_update <- ff_update_county(gender_pay_data, table_number, 'NC', 'Forsyth', 2016)
+# 
+# # write out modified dataset that contains past and current data
+# write_csv(gender_update, gender_pay_data)
