1a0a6f7ae7c3b99e06e07e4b78a6703996189547
shanejorr <shane.j.orr@gmail.com>
2018-08-21 17:41:03 -0400

    Added function that calculates ratios and the moe of the ratio. Created Rmarkdown template for displaying data. Output of RMarkdown template is also included.

--- acs_functions.R
+++ acs_functions.R
@@ -224,6 +224,32 @@ ff_import_acs_county <- function(table_number, state, county, year_start, year_e
 
 ######################################################
 
+ff_ratios <- function(df, num_estimate, num_moe, den_estimate, den_moe) {
+  
+  # This function calculates the 95% margins of error for user generated rations
+  # the inputs are column names from the dataframe, entered as strings
+  # the formula comes from:
+  #    U.S. Census Bureau, A Compass for Understanding and Using ACS Survey Data, A-15
+  
+  # The output is the same dataframe with the ratio and margin of error added to the dataframe
+  
+  # calculate the ratio
+  ratio  <- df[[num_estimate]] / df[[den_estimate]]
+  
+  # calculate the numerator for the MOE formula
+  moe_num <- sqrt(df[[num_moe]]^2 + (ratio^2 * df[[den_moe]]^2))
+  
+  # calculate MOE
+  moe <- moe_num / df[[den_estimate]]
+  
+  # add ratio and MOE to dataframe
+  df$ratio <- ratio
+  df$ratio_moe <- moe
+  
+  return(df)
+  
+}
+
 ########### Misc  ###########
 
 # The following functions are miscellaneous
