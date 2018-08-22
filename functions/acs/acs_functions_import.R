73655add2407bf452c6d1c3d6512f85aa6550ef3
shanejorr <shane.j.orr@gmail.com>
2018-08-22 17:18:16 -0400

    Create file and function to easily import all custom functions.

--- functions/acs/acs_functions_import.R
+++ functions/acs/acs_functions_import.R
@@ -211,3 +211,4 @@ ff_import_acs <- function(table_number, state, county, year_start, year_end) {
   return(geo_first)
   
 }
+
--- functions/acs_load_funcs.R
+++ functions/acs_load_funcs.R
@@ -3,8 +3,14 @@
 # This file creates and runs a function to load all ACS functions at once.
 # Running this file in an R script will load all ACS functions.
 #
+# Two different functions must be used. The first one will work with markdown,
+# the next one will work with R scripts.
+#
+# They are both needed because the working directory changes in markdown files.
+#
 #########################################################################
 
+##For markdown files
 # use a function so the global envirnment is not littered with objects
 acs_load_func <- function() {
   
@@ -16,6 +22,28 @@ acs_load_func <- function() {
     
     # create path to file
     file_path <- paste0('../functions/acs/', f)
+    
+    # run file
+    source(file_path)
+    
+  }
+}
+
+# run function
+acs_load_func()
+
+##For scripts
+# use a function so the global envirnment is not littered with objects
+acs_load_func <- function() {
+  
+  # locate all files within the ACS functions folder
+  files <- list.files('functions/acs')
+  
+  # run all the files containing ACS functions
+  for (f in files) {
+    
+    # create path to file
+    file_path <- paste0('functions/acs/', f)
     
     # run file
     source(file_path)
