d28981140fa12fab8af9c2ff192e67f9720b1aa3
shanejorr <shane.j.orr@gmail.com>
2018-09-04 17:38:21 -0400

    update palma

--- i_social_justice/palma.R
+++ i_social_justice/palma.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(data.table)
+library(pryr)
 
 # Create housing variables
 
@@ -16,21 +17,21 @@ equivalence_scale <- function(num_adults, num_children) {
         num_adults <= 2 & num_children == 0, num_adults^0.5,
             ifelse(# if single parent
               num_adults == 1 & num_children >= 1,
-                (num_adults + 0.8 * 1 + 0.5 * num_children-1)^0.7,
-              # other families
-              (num_adults + 0.5 * num_children)^0.7
-            )
+                (num_adults + 0.8 * 1 + 0.5 * (num_children-1))^0.7,
+                  # other families
+                  (num_adults + 0.5 * num_children)^0.7
+              )
             )
   
 }
 
 ### import data ###
-housing <- fread('ss14husa.csv', select = house_vars) %>%
+housing <- fread('pums/ss14husa.csv', select = house_vars) %>%
   # filter for housing units and for units with reported household income
   filter(RT == 'H',
-         !is.null(HINCP)) %>%
+         !is.na(HINCP)) %>%
   # merge housing with population data
-  inner_join(fread('ss14pusa.csv', select = pop_vars),
+  inner_join(fread('pums/ss14pusa.csv', select = pop_vars),
              by = 'SERIALNO') %>%
   # create categorical variable on whether person is adult or child
   mutate(status = ifelse(.$AGEP >= 18, 'adult', 'child'))
@@ -49,7 +50,9 @@ house_num <- housing %>%
          child = replace_na(child, 0)) %>%
   # replace NA values (which represent 0 adults or children) with 0
   mutate(adult = replace_na(adult, 0),
-         child = replace_na(child, 0))
+         child = replace_na(child, 0)) %>%
+  # ungroup to save RAM
+  ungroup()
 
 
 # add number of adults and children to primary housing data frame
@@ -61,14 +64,34 @@ housing <- housing %>%
   distinct() %>%
   # add data frame that shows number of adults and children
   inner_join(house_num, by = 'SERIALNO') %>%
-  # ungroup to save RAM
-  ungroup()
+  # create equivalence scale
+  mutate(eq_scale = equivalence_scale(.$adult, .$child)) %>%
+  # multiply income by exquivalency scale
+  mutate(income = HINCP / eq_scale) %>%
+  # drop children and adult columns
+  select(-adult, -child)
 
 # house_num no longer needed since it was joined to house
 rm(house_num)
 gc()
-  
 
-house_light <- head(housing, 100)
+object_size(housing)
 
-equivalence_scale(house_light$adult, house_light$child)
+# convert to data.table for the rest of the calculation
+housing <- data.table(housing)
+object_size(housing)
+
+# extend dataframe based on weights
+housing <- housing[ ,list(freq=rep(1,WGTP)),by=c("SERIALNO", 'income')][
+  , c('freq', 'SERIALNO') := NULL][ # delete columns
+    order(-income)] # order descending
+
+# total number of households; used to find percentiles
+n_houses <- housing[, .N]
+
+# total earnings of bottom 40% and top 10%
+bottom <- housing[1:floor(n_houses*.40), sum(income)]
+top <- housing[floor(n_houses*.90):n_houses, sum(income)]
+
+housing <- housing[order(income)] # order by income
+housing[income <= 1, .N] / housing[, .N]  
