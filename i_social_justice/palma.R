d583a1c27c506de5474188b90dda9d0fe754031b
Shane Orr <36132078+shanejorr@users.noreply.github.com>
2018-09-10 10:52:57 -0400

    calculate palmas for geographies

--- i_social_justice/palma.R
+++ i_social_justice/palma.R
@@ -4,20 +4,27 @@ library(data.table)
 
 source('i_social_justice/palma_functions.R')
 source('functions/puma_functions.R')
-#source('functions/comparison_counties.R')
+source('functions/comparison_counties.R')
 
 
 # create dataframe of NC's ten largest counties
-#counties <- load_comparisons()
+counties <- load_comparisons()
 
 # get area codes of ten largest counties minus Forsyth
-#area_codes <- puma_area_code(counties$county[-1], 'puma_counties.csv', puma12 = TRUE)
+area_codes <- puma_area_code('Forsyth', 'puma_counties.csv', puma12 = TRUE)
 
-state = NA #37
-area_code = NA # area_codes$PUMA
-years = c('06', '07', '08', '09', '10', '11')
+state = 37
+area_code = area_codes$PUMA
+years = c('12', '13', '14', '15', '16')
 data_directory = 'pums'
 
 
 urban_palma <- palma_years(state = state, area_code = area_code, 
                             years = years, data_directory)
+
+# create dataset of all Palmas
+palmas <- read_csv('i_social_justice/data/us_palma.csv') %>%
+  bind_rows(read_csv('i_social_justice/data/urban_palma.csv')) %>%
+  bind_rows(read_csv('i_social_justice/data/forsyth_palma.csv'))
+
+write_csv(palmas, 'i_social_justice/data/palmas.csv')
