69ed5b19dd039ea2026a1f5f54ecfba30fef6d5f
shanejorr <shane.j.orr@gmail.com>
2018-09-10 14:32:47 -0400

    change table of variable names to include S tables

--- acs_variable_names.R
+++ acs_variable_names.R
@@ -4,7 +4,7 @@
 # to repeatedly call the census api everytime a list of variable names is needed.
 #
 # This file should be updated to the current year when new ACS data drops.
-# TEST
+# 
 ########################################################################################################
 
 
@@ -13,6 +13,13 @@ library(tidycensus)
 
 current_year <- 2016
 
-var_names <- load_variables(current_year, "acs1")
+# import b tables
+vars_b <- load_variables(current_year, "acs1")
 
-write_csv(var_names, 'acs_variable_names.csv')
+# import s tables
+vars_s <- load_variables(2016, "acs1/subject")
+
+# bind b and s table names into one tables
+vars <- bind_rows(vars_b, vars_s)
+
+# write_csv(vars, 'acs_variable_names.csv')
