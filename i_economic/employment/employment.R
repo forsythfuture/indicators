09f2d13fe8604d0c1c46b8a7ce299566c11797e0
shanejorr <shane.j.orr@gmail.com>
2018-09-13 12:08:12 -0400

    Update for 2017 data; Change improt function so that original data is not deleted

--- i_economic/employment/employment.R
+++ i_economic/employment/employment.R
@@ -5,20 +5,16 @@
 library(tidyverse)
 source('functions/misc_functions.R')
 
+# folder to put raw data into
+data_path <- 'i_economic/employment/data/raw_data'
+
+# full path and file name of zip file
+zip_file <- 'zip_files/employment_all_counties_years.zip'
+
 # import and clean employment data
-df <- ff_import_acs('zip_files/aff_download.zip', 
-                    'zip_files', years = seq(2006, 2016, 1))
+df <- ff_import_acs(zip_file,
+                    data_path, 
+                    years = seq(2006, 2017, 1))
 
 # write out data frame
 write_csv(df, 'i_economic/employment/data/employment_all_years_counties.csv')
-
-## import Forsyth census tract data for 2016
-
-# unzip census tract files
-unzip_files('aff_download.zip', file_folder)
-
-# clean census tract files
-tracts <- clean_acs(paste0(file_folder, 'ACS_16_5YR_S2301_with_ann.csv'), 2016)
-
-# write out data frame
-#write_csv(tracts, paste0(file_folder, 'employment_tracts.csv'))
--- i_economic/median_income/median_income.R
+++ i_economic/median_income/median_income.R
@@ -8,50 +8,16 @@
 library(tidyverse)
 source('functions/misc_functions.R')
 
-### import each year's data and bind to previous year
+# folder to put raw data into
+data_path <- 'i_economic/median_income/data/raw_data'
 
-## import US, NC state and NC county data
+# full path and file name of zip file
+zip_file <- 'zip_files/median_income.zip'
 
-# 2006-2016 is in a zip file, unzip files and add to folder
-
-file_folder = 'i_economic/median_income/data'
-
-unzip_files('zip_files/median_income.zip', file_folder)
-
-# list of files (each file represents a year of data)
-data_files <- paste0(file_folder, '/', list.files(file_folder))
-
-# list of years
-years <- seq(2006, 2016, 1)
-
-### iterate through each file and year, extract data, and bind to previous year
-
-# initialize dataframe
-emp <- data.frame()
-
-for (i in seq_along(data_files)) {
-  
-  # import one year of data
-  df <- clean_acs(data_files[i], years[i])
-  
-  # bind to previous year
-  emp <- emp %>%
-    bind_rows(df)
-}
-
-# remove individual yearly files
-file.remove(data_files)
+# import and clean employment data
+df <- ff_import_acs(zip_file,
+                    data_path, 
+                    years = seq(2006, 2017, 1))
 
 # write out data frame
-write_csv(emp, paste0(file_folder, '/', 'median_income_all_years_counties.csv'))
-
-## import Forsyth census tract data for 2016
-
-# unzip census tract files
-unzip_files('aff_download.zip', file_folder)
-
-# clean census tract files
-tracts <- clean_acs(paste0(file_folder, 'ACS_16_5YR_S2301_with_ann.csv'), 2016)
-
-# write out data frame
-#write_csv(tracts, paste0(file_folder, 'employment_tracts.csv'))
+write_csv(df, 'i_economic/median_income/data/median_income.csv')
