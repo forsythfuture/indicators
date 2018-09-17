8468bcfb750aaffd95d325da96d864ff34c2fca7
shanejorr <shane.j.orr@gmail.com>
2018-09-17 17:40:21 -0400

    amend function for calculating ratios create employment ratio markdown file

--- functions/acs/acs_functions_test.R
+++ functions/acs/acs_functions_test.R
@@ -8,34 +8,41 @@
 ##############################################################################
 
 
-ff_acs_ratios <- function(df, num_estimate, num_moe, den_estimate, den_moe) {
+ff_acs_ratios <- function(num_estimate, num_moe, den_estimate, den_moe) {
   
   # This function calculates the ratios, se, and 95% margins of error for the ratios
-  # The inputs are column names from the dataframe, entered as strings
+  # The inputs are as follows:
+  #   num_estimate: The dataframe and column of the numerator in ratio
+  #   num_moe: The dataframe and column of the numerator's moe
+  #   den_estimate: The dataframe and column of the denominator in ratio
+  #   den_moe: The dataframe and column of the denominator's moe
+  #
   # The formula comes from:
   #    U.S. Census Bureau, A Compass for Understanding and Using ACS Survey Data, A-15
   #
   # The output is the same dataframe with the ratio and margin of error added to the dataframe
   
-  # calculate the ratio
-  ratio  <- df[[num_estimate]] / df[[den_estimate]]
-  
-  # calculate the numerator for the MOE formula
-  moe_num <- sqrt(df[[num_moe]]^2 + (ratio^2 * df[[den_moe]]^2))
+  # calculate the ratio 
+  ratio  <- num_estimate / den_estimate
   
   # calculate MOE
-  moe <- moe_num / df[[den_estimate]]
+  # uses the tidycensus package
+  moe <- moe_ratio(num_estimate, den_estimate, num_moe, den_moe)
   
   # calcuate standard error
   # note: since 95% confidence intervals are used, the moe is divided
   # by 1.96, not 1.645 when using the 90% moe's from AFF
   se <- moe / 1.96
   
-  # add ratio, MOE, and se to dataframe
-  df$ratio <- ratio
-  df$ratio_moe <- moe
-  df$ratio_se <- se
+  # calculate cv
+  cv <- (ratio / se) * 100
   
+  # add ratio, MOE, se and cv to dataframe
+  df <- data.frame(ratio = ratio,
+                   moe = moe,
+                   se = se,
+                   cv = cv)
+
   return(df)
   
 }
