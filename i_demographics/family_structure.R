a111044153ec8ad5a982416c73c6e261af10bf43
cyerat <cyera.taylor@salem.edu>
2018-10-04 11:49:55 -0400

    family structure files

--- i_demographics/family_structure.R
+++ i_demographics/family_structure.R
@@ -8,13 +8,14 @@ library(tidyverse)
 source('functions/acs/acs_misc_functions.R')
 
 # folder to put raw data into
-data_path <- 'i_economic/poverty/data/raw_data'
+data_path <- 'i_demographics/data/raw_data'
 
 # full path and file name of zip file
-zip_file <- 'zip_files/poverty.zip'
+zip_file <- 'zip_files/Family_Structure_Hispanic_DC.zip'
 
 # import and clean employment data
-df <- ff_import_acs(zip_file,
+
+Family_Structure_Hispanic_DC<- ff_import_acs(zip_file,
                     data_path, 
                     years = seq(2006, 2017, 1))
 
