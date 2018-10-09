7d565bab5af13d22cd9c57e7a6e368b59a3dcb74
Cyera Taylor <cyera.taylor@salem.edu>
2018-10-09 09:29:43 -0400

    updates

--- i_demographics/family_structure.R
+++ i_demographics/family_structure.R
@@ -3,7 +3,7 @@
 # This script imports and cleans poverty tables
 #
 ########################################################
-
+install.packages('tidyverse')
 library(tidyverse)
 source('functions/acs/acs_misc_functions.R')
 
@@ -11,17 +11,17 @@ source('functions/acs/acs_misc_functions.R')
 data_path <- 'i_demographics/data/raw_data'
 
 # full path and file name of zip file
-zip_file <- 'zip_files/Family_Structure_Hispanic_DC.zip'
+zip_file <- 'zip_files/family_structure_total.zip'
 
 # import and clean employment data
 
-Family_Structure_Hispanic_DC<- ff_import_acs(zip_file,
+df<- ff_import_acs(zip_file,
                     data_path, 
                     years = seq(2006, 2017, 1))
 
 # write out data frame
-#write_csv(df, 'i_economic/poverty/data/poverty_all_years_counties.csv')
-
+write_csv(df, 'i_demographics/data/family_structure_hl_years_counties.csv')
+write_csv(df, 'i_economic/poverty/data/poverty_age_groups.csv')
 ### age data is in a different table
 
 # folder to put raw data into
