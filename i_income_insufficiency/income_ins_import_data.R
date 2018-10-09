ded10989a3f0c233a3a9f9b53bcc4ee005c8733a
shanejorr <shane.j.orr@gmail.com>
2018-10-09 17:22:06 -0400

    gather and clean ces and meps data for income insufficiency

--- i_income_insufficiency/income_ins_import_data.R
+++ i_income_insufficiency/income_ins_import_data.R
@@ -1,108 +1,42 @@
 library(tidyverse)
+library(readxl)
 
-### consumer expenditure survey ###
+source('i_income_insufficiency/income_ins_functions.R')
 
-#######################################################################################################
+####################### consumer expenditure survey #############################
 
-clean_ces <- function(data_file, header_file) {
-  
-  # import data
-  df <- read_delim(data_file,
-                      delim = '\t') %>%
-    # convert from wide (each column is a different year) 
-    # to long (each row is a different year and expense)
-    gather("Year", 'Expense', `Annual 2006`:`Annual 2017`) %>%
-    # remove word 'Annual' from year column
-    mutate(Year = str_replace_all(Year, 'Annual ', '')) %>%
-    # trim whitespace in all columns
-    # need to trim so values in this column can match values in dataframe containing estimates
-    mutate_all(funs(str_trim(., side = 'both'))) %>%
-    # convert years and expense estimtae to numeric
-    mutate_at(c('Expense', 'Year'), as.numeric)
-  
-  # import headers
-  df_headers <- read_delim(header_file,
-                              delim = '\t',
-                              col_names = FALSE) %>%
-    # only keep headers
-    filter(!str_detect(X1, '^CXU'),
-           # remove rows that jsut say 'Series ID'
-           X1 != 'Series Id') %>%
-    # separate columsn at ':', this puts header title and header value in different columns
-    separate(X1, into = c('titles', 'values'), sep = ': ', extra = 'merge', remove = TRUE) %>%
-    # add column that adds an ID number to each series group
-    # needed to spread columns
-    mutate(id = rep(1:(nrow(.)/6), each = 6)) %>%
-    # convert from long to wide where each header title is in a different row
-    spread(titles, values) %>%
-    # remove unneeded values
-    select(-id, -Category) %>%
-    # rename to match column name in dataframe containing data
-    # we will merge on this column, so names need to be the same
-    rename(`Series ID` = `Series Id`) %>%
-    # trim whitespace in all columns
-    # need to trim so values in this column can match values in dataframe containing estimates
-    mutate_all(funs(str_trim(., side = 'both')))
-  
-  # combine header descriptions to dataframe containing expenses dat
-  df <- left_join(df, df_headers, by = 'Series ID')
-  
-  ### adjust expenses of number of people in house by region (south)
-  
-  ###################################################################################################
-  # The data shows average national expenses by type, averge region expenses, 
-  # and average expenses by number of poepl in household.
-  # But, there is no data for expenses by average number of people in household and region.
-  # Therefore, we will adjust household expenses by the ration of the south to the national average
-  # in each expense category
-  ##################################################################################################
-  
-  # create ratio of south to the rest of the United States by splitting entire US and South into different datasets,
-  # then merging; this puts each region's estimate on the same row
-  
-  us <- df %>%
-    filter(Characteristics == 'All Consumer Units') %>%
-    select(Item, Year, Expense)
-  
-  south <- df %>%
-    filter(Characteristics == 'Region of residence: south') %>%
-    select(Item, Year, Expense)
-  
-  # merge (must join by item because series ID is different depending on region and num of people in houshold)
-  ratio <- left_join(us, south, by = c('Item', 'Year')) %>%
-    # calculate ratio by dividing South estimate by US estimate
-    mutate(region_ratio = Expense.y / Expense.x) %>%
-    # drop column not needed to join ratio to dataset containing estimate
-    select(Item, Year, region_ratio)
-  
-  # remove region rows from dataset contining expenses, because we only needed
-  # regions to calculate ratio
-  df <- df %>%
-    filter(!(Characteristics == 'All Consumer Units' |
-               Characteristics == 'Region of residence: south')) %>%
-    # add region adjusted ratios
-    left_join(ratio, by = c('Item', 'Year')) %>%
-    # multiply expense by region ratio, round to nearest whole number
-    mutate(Expense = round( Expense*region_ratio, 0 )) %>%
-    # ratios are no longer needed
-    select(-region_ratio)
-  
-  return(df)
-}
+########## data source ######################################################################
+#
+# All consumer expenditure survey (ces) data was gathered from the BLS website containing
+# consumer expenditure data. The following expenses were collected fro mCES data: 
+# health insurance (by household size annd age), transportation, appareal and services, housekeeping supplies
+# personal care products and services, reading, and misc. 
+#
+# To collect all expenses except for health insurance by age, the one screen data source application
+# was used (https://data.bls.gov/PDQWeb/cx). From this page, the 'Expenses' Category was selected.
+# Next, the following subcategories were used 
 
-######################################################################################
 
 # create path to folder with data (to shorten file names that have to be entered)
-
-data_path <- 'i_income_insufficiency/data/'
+data_path <- 'i_income_insufficiency/data/ces/'
 
 ### health insurance for households
-# health insurance for poeple over 65 is in a different table
-health_household <- clean_ces(paste0(data_path, 'health/health_ins_household_raw_data.txt'),
-                              paste0(data_path, 'health/health_ins_household_headers.txt'))
+# health insurance for poeple over 65 is in a different table and is not included here
+health_household <- clean_ces(paste0(data_path, 'health_ins_household_raw_data.txt'),
+                              paste0(data_path, 'health_ins_household_headers.txt'))
 
 ### transportation
-trans <- clean_ces(paste0(data_path, 'transportation/transportation_raw_data.txt'),
-                   paste0(data_path, 'transportation/transportation_headers.txt'))
+trans <- ff_clean_ces(paste0(data_path, 'transportation_raw_data.txt'),
+                      paste0(data_path, 'transportation_headers.txt'))
+
+### health insurance 65+
+health_age <- read_excel(paste0(data_path, '/health_ins_age/health_ins_age_16.xlsx'), skip = 2) %>%
+  # only select items column and two age columns
+  select(Item, starts_with('65-74'), starts_with('75 years')) %>%
+  # trim whitespace in all columns
+  mutate_all(funs(str_trim(., side = 'both'))) %>%
+  # only keep row showing health insurance expenses
+  filter(Item == 'Health insurance')
+
 
 
