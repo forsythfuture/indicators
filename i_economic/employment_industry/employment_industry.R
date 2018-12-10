76955207c55ec9f9d06c98e346ab48b2de348679
shanejorr <shane.j.orr@gmail.com>
2018-12-10 18:34:59 -0500

    update employment by industry

--- i_economic/employment_industry/employment_industry.R
+++ i_economic/employment_industry/employment_industry.R
@@ -3,7 +3,6 @@ library(tidyverse)
 # pound signs are not present in data
 industry_titles <- c('Management of companies and enterprises',
                      'Finance and insurance',
-                     'Professional scientific and technical services',
                      'Professional and technical services',
                      'Manufacturing',
                      'Wholesale trade',
@@ -12,23 +11,28 @@ industry_titles <- c('Management of companies and enterprises',
                      'Transportation and warehousing',
                      'Public administration',
                      'Construction',
-                     'Administrative and support and waste management and remediation services',
-                     'Administrative and waste services',
+                     'Administrative and support services',
                      'Retail trade',
-                     'Other services (except public administration)',#
+                     'Other services',
                      'Accommodation and food services')
 
 # read in both years data
-emp <- read_csv('i_economic/employment_industry/forsyth_jobs_industry_08.csv') %>%
+empA <- read_csv('i_economic/employment_industry/forsyth_jobs_industry_08.csv') %>%
   bind_rows(read_csv('i_economic/employment_industry/forsyth_jobs_industry_17.csv')) %>%
   # some of the industry descriptions have 'NAICS' and numbers; remove these
-  mutate(industry_title = str_replace_all(industry_title, 'NAICS |[0-9]|[0-9] |^[-]', "")) %>%
+  mutate(industry_title = str_trim( str_replace_all(industry_title, 'NAICS |\\d|[-]', ""), side = 'both')) %>%
   select(year, area_title:industry_title,
          annual_avg_emplvl, # average annual monthly employment
          avg_annual_pay # average annual pay
          ) %>%
-  #filter(industry_title %in% industry_titles) %>%
+  filter(industry_title %in% industry_titles) %>%
+  # dataset has rows for public and private employment by industry
+  # group public and private, and sum for total employment by industry
+  group_by(c('year', 'industry_title')) %>%
+  summarize()
+  
   unique()
 
+emp %>% filter(year == 2017)
 emp %>% filter(str_detect(industry_title, 'Management'))
 # vector of the columns that are needed
