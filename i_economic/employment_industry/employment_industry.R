676426d8abc216d66e943464536d739c90758155
Shane Orr <shane.j.orr@gmail.com>
2018-12-10 22:57:48 +0000

    update employment by industry

--- i_economic/employment_industry/employment_industry.R
+++ i_economic/employment_industry/employment_industry.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 
 # pound signs are not present in data
-industry_titles <- c('Management of companies and interprises',#
+industry_titles <- c('Management of companies and enterprises',
                      'Finance and insurance',
-                     'Professional scientific and technical services',#
+                     'Professional scientific and technical services',
+                     'Professional and technical services',
                      'Manufacturing',
                      'Wholesale trade',
                      'Educational services',
@@ -11,20 +12,23 @@ industry_titles <- c('Management of companies and interprises',#
                      'Transportation and warehousing',
                      'Public administration',
                      'Construction',
-                     'Administrative and support and waste management and remediation services',#
+                     'Administrative and support and waste management and remediation services',
+                     'Administrative and waste services',
                      'Retail trade',
                      'Other services (except public administration)',#
                      'Accommodation and food services')
 
 # read in both years data
 emp <- read_csv('i_economic/employment_industry/forsyth_jobs_industry_08.csv') %>%
-  bind_rows(read_csv('i_economic/employment_industry/forsyth_jobs_industry_08.csv')) %>%
+  bind_rows(read_csv('i_economic/employment_industry/forsyth_jobs_industry_17.csv')) %>%
+  # some of the industry descriptions have 'NAICS' and numbers; remove these
+  mutate(industry_title = str_replace_all(industry_title, 'NAICS |[0-9]|[0-9] |^[-]', "")) %>%
   select(year, area_title:industry_title,
          annual_avg_emplvl, # average annual monthly employment
          avg_annual_pay # average annual pay
          ) %>%
-  filter(industry_title %in% industry_titles) %>%
+  #filter(industry_title %in% industry_titles) %>%
   unique()
 
-
+emp %>% filter(str_detect(industry_title, 'Management'))
 # vector of the columns that are needed
